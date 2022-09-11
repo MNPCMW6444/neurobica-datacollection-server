844fe4c81bfed53456f5de51077dde051a3f6530
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-09-11 10:59:55 +0300

    awfaqwf

--- models/museDataMocel.js
+++ models/museDataMocel.js
@@ -1,16 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
-    },
-    passwordHash: { type: String, required: true },
-    token: {
-      type: Array,
-    },
-  },
+  {},
 
   {
     timestamps: true,
